3028ca45a2950fa117c6d5be90326a69f73262fc
Mangus <enjoyandrey2@gmail.com>
2023-09-18 11:46:26 +0300

    Change save method to native

--- src/hooks/ref/useDownloadingImage.ts
+++ src/hooks/ref/useDownloadingImage.ts
@@ -1,22 +1,24 @@
 import React, { useEffect } from "react";
 import { setIsDownloading } from "@/redux/slices/canvas/reducer";
 import { useAppDispatch, useAppSelector } from "@/redux/store";
-import { IElement } from "@/types/canvas";
 import { downloadURI } from "@/utils/downloadURI";
-import { getCanvasElementProps } from "@/utils/getCanvasElementProps";
 import Konva from "konva";
 
 import Stage = Konva.Stage;
+import Group = Konva.Group;
 
 export const useDownloadingImage = (stageRef: React.RefObject<Stage>) => {
   const dispatch = useAppDispatch();
-  const { isDownloading, elements } = useAppSelector(state => state.canvas);
+  const { isDownloading } = useAppSelector(state => state.canvas);
   const browser = useAppSelector(state => state.browser);
 
   useEffect(() => {
     if (!isDownloading) return;
-    if (!stageRef.current || !stageRef.current.children) return;
-    const uri = createPseudoLayer(elements).toDataURL({
+    const layerChildren = stageRef.current?.children;
+    if (!layerChildren || !layerChildren[1].children) return;
+    const group: Group = layerChildren[1].children[0].clone();
+    group.scale({ x: 1, y: 1 });
+    const uri = group.toDataURL({
       width: browser.layerWidth,
       height: browser.layerHeight,
       pixelRatio: 1
@@ -24,50 +26,4 @@ export const useDownloadingImage = (stageRef: React.RefObject<Stage>) => {
     downloadURI(uri, "image");
     dispatch(setIsDownloading(false));
   }, [isDownloading]);
-};
-
-const createPseudoLayer = (elements: IElement[]) => {
-  const layer = new Konva.Layer();
-
-  elements.forEach(element => {
-    let shape = getShape(element);
-    // @ts-ignore
-    layer.add(shape);
-  });
-
-  return layer;
-};
-
-const getShape = (element: IElement) => {
-  const { tool, ...elementProps } = element;
-  switch (tool) {
-    case "rect":
-      return new Konva.Rect({
-        ...getCanvasElementProps(element),
-        ...elementProps
-      });
-    case "ellipse":
-      // @ts-ignore
-      return new Konva.Ellipse({
-        ...getCanvasElementProps(element),
-        ...elementProps
-      });
-    case "pen":
-    case "eraser":
-      return new Konva.Line({
-        ...getCanvasElementProps(element),
-        ...elementProps
-      });
-    case "text":
-      return new Konva.Text({
-        ...getCanvasElementProps(element),
-        ...elementProps
-      });
-    case "image":
-      // @ts-ignore
-      return new Konva.Image({
-        ...getCanvasElementProps(element),
-        ...elementProps
-      });
-  }
 };
